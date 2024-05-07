7e379ec4419473de24f87f79a0a155f086c19cdc
Nycolas Padilha <763npp@gmail.com>
2024-05-07 09:14:16 -0300

    chore(homepage): time progress + page skeleton

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,40 @@ export default function Home() {
     <section id='homepage'>
       <div className='banner'>PlaceHolder</div>
       <section className='top'>
-        <div className='time-progress'></div>
+        <div className='time-progress'>
+          <iframe
+            src="https://indify.co/widgets/live/progressBar/OGDIEMrNCVgIdbGcwx3B"
+            className='progress-bar'
+          />
+        </div>
         <div className='profile'></div>
       </section>
       <section className='content'>
-        <div className='skills'></div>
-        <div className='central'></div>
+        <section className='first-section'>
+          <div className='skills'>
+            <h1>Skills</h1>
+            <div className='skills-graphic'>PlaceHolder</div>
+          </div>
+          <div className='central'>
+            <h1>Central</h1>
+            <section className='central-content'>
+              <div className='card'></div>
+              <div className='card'></div>
+              <div className='card'></div>
+              <div className='card'></div>
+              <div className='card'></div>
+              <div className='card'></div>
+              <div className='card'></div>
+              <div className='card'></div>
+            </section>
+          </div>
+        </section>
+        <section className='second-section'>
+          <h1>- Habitos -</h1>
+        </section>
+        <section className='third-section'>
+          <h1>- Shop -</h1>
+        </section>
       </section>
     </section>
   );
